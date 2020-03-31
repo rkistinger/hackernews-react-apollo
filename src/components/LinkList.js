a9eef84f255aab731bd4a330d72ad14b05345f60
Ross Kistinger <rkistinger12@gmail.com>
2020-03-30 22:17:23 -0500

    finished

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,10 +3,11 @@ import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
 
 import Link from './Link'
+import { LINKS_PER_PAGE } from '../constants'
 
 export const FEED_QUERY = gql`
-  {
-    feed {
+  query FeedQuery($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(first: $first, skip: $skip, orderBy: $orderBy) {
       links {
         id
         createdAt
@@ -23,23 +24,147 @@ export const FEED_QUERY = gql`
           }
         }
       }
+      count
+    }
+  }
+`
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription NewLinksSubscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
     }
   }
 `
 
 export default function LinkList(props) {
-  const { loading, error, data } = useQuery(FEED_QUERY)
+  const isNewPage = props.location.pathname.includes('new')
+  const page = parseInt(props.match.params.page, 10)
+  const pageIndex = props.match.params.page ? (page - 1) * LINKS_PER_PAGE : 0
+  const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+  const first = isNewPage ? LINKS_PER_PAGE : 100
+  const orderBy = isNewPage ? 'createdAt_DESC' : null
+
+  const { loading, error, data, subscribeToMore } = useQuery(FEED_QUERY, {
+    variables: {
+      skip,
+      first,
+      orderBy,
+    },
+  })
+
+  // Update the cached data for this query when a specified subscription fires
+  subscribeToMore({
+    // Specify the subscription that we want to react to
+    document: NEW_LINKS_SUBSCRIPTION,
+    // Similar to cache update prop, this function allows you to determine how the store
+    // should be updated with the information that was sent by the server after the event occurred.
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev
+
+      const newLink = subscriptionData.data.newLink
+      const exists = prev.feed.links.find(({ id }) => id === newLink.id)
+      if (exists) return prev
+
+      // Add data returned from subscription to the existing cached data for FEED_QUERY
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename,
+        },
+      })
+    },
+  })
+
+  subscribeToMore({
+    document: NEW_VOTES_SUBSCRIPTION,
+  })
 
   if (loading) return <div>Fetching</div>
   if (error) return <div>Error</div>
 
-  const linksToRender = data.feed.links
+  const getLinksToRender = () => {
+    if (isNewPage) {
+      return data.feed.links
+    }
+
+    const rankedLinks = data.feed.links.slice()
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+    return rankedLinks
+  }
+
+  const canGoNext = page <= data.feed.count / LINKS_PER_PAGE
+  const canGoPrev = page > 1
 
   return (
     <div>
-      {linksToRender.map((link, index) => (
-        <Link key={link.id} link={link} index={index} />
+      {getLinksToRender().map((link, index) => (
+        <Link key={link.id} link={link} index={index + pageIndex} />
       ))}
+
+      {isNewPage && (
+        <div className="flex ml4 mv3 gray">
+          <button
+            className="pointer mr2"
+            disabled={!canGoPrev}
+            onClick={() => {
+              props.history.push(`/new/${page - 1}`)
+            }}
+          >
+            Previous
+          </button>
+          <button
+            className="pointer"
+            disabled={!canGoNext}
+            onClick={() => {
+              props.history.push(`/new/${page + 1}`)
+            }}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,97 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { BrowserRouter } from 'react-router-dom'
+// import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink, split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 import './index.css'
 import App from './components/App'
 import { AUTH_TOKEN } from './constants'
 
-// Configure the apollo client with the endpoint of our GraphQL server
-// apollo-boost provides defaults client configuration out of the box
-// https://www.apollographql.com/docs/react/get-started/#configuration-options
-const client = new ApolloClient({
+/*
+  Configure the apollo client with the endpoint of our GraphQL server.
+  Apollo Boost provides defaults client configuration out of the box (but allows for minimal configuration).
+  https://www.apollographql.com/docs/react/get-started/#configuration-options
+ */
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000',
+//   // Set token as header on each request
+//   request: (operation) => {
+//     const token = localStorage.getItem(AUTH_TOKEN)
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : '',
+//       },
+//     })
+//   },
+// })
+
+/*
+  Apollo Boost is a great way to get started with Apollo Client quickly,
+  but there are some advanced features it doesn't support out of the box.
+
+  If you'd like to use subscriptions, swap out the Apollo cache,
+  or add an existing Apollo Link to your network stack that isn't already included,
+  you will have to set Apollo Client up manually.
+*/
+const authLink = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  const token = localStorage.getItem(AUTH_TOKEN)
+
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }))
+
+  return forward(operation)
+})
+
+const httpLink = new HttpLink({
   uri: 'http://localhost:4000',
-  // Set token as header on each request
-  request: (operation) => {
-    const token = localStorage.getItem(AUTH_TOKEN)
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    })
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      // Authenticate the websocket connection
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
   },
 })
 
-// If necessary, we can execute queries / mutations directly on the client
+// Using the ability to split links, you can “route” a request to
+// a specific middleware link depending on what kind of operation is being sent
+const link = split(
+  // Test function:
+  // If test returns true, the request will be forwarded to the link passed as the second argument.
+  // If false, to the third one
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    // Route subscriptions to WS link. Queries & mutations to HTTP link.
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
+const client = new ApolloClient({
+  // Apollo links are middlewares used in the client's networking layer
+  link,
+  // Recommended default cache
+  cache: new InMemoryCache(),
+})
+
+/* If necessary, we can execute queries / mutations directly on the client */
 // client
 //   .query({
 //     query: gql`
